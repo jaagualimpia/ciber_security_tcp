f534358e16d2ef1ab5ecd3e2996c6bcc8001caf2
jaagualimpia <jaagualimpia25@gmail.com>
2024-03-11 07:57:11 -0500

    Bugs fixed

--- ReconocimientoFacial.py
+++ ReconocimientoFacial.py
@@ -5,84 +5,41 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def detectFace():
-	dataPath = os.environ["DATA_PATH_FINAL"] #Cambia a la ruta donde hayas almacenado Data
-	imagePaths = os.listdir(dataPath)
-	print('imagePaths=',imagePaths)
+    face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+    face_recognizer.read('modeloLBPHFace.xml')
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+    captures = 0
 
-	face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-	face_recognizer.read('modeloLBPHFace.xml')
-	cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
-	faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
-	captures = 0
+    while True:
+        ret, frame = cap.read()
+        if ret == False:
+            break
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        auxFrame = gray.copy()
 
-	while True:
-		ret, frame = cap.read()
-		if ret == False: break
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		auxFrame = gray.copy()
+        faces = faceClassif.detectMultiScale(gray, 1.3, 5)
 
-		faces = faceClassif.detectMultiScale(gray,1.3,5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-		for (x,y,w,h) in faces:
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-		
-		captures += 1
+        captures += 1
 
-		print(captures)
+        if len(faces) != 0:
+            rostro = auxFrame[y:y+h, x:x+w]
+            rostro = cv2.resize(rostro, (150, 150),
+                                 interpolation=cv2.INTER_CUBIC)
+            result = face_recognizer.predict(rostro)
+            			
+            if result[1] < 60:
+                print(result[1])
+                return True
 
-		if len(faces) != 0:
-			rostro = auxFrame[y:y+h,x:x+w]
-			rostro = cv2.resize(rostro,(150,150),interpolation= cv2.INTER_CUBIC)
-			result = face_recognizer.predict(rostro)
+        if captures == 35:
+            break
 
-			if result[0] < 70:
-				return True
-
-		if captures == 35:
-			break 
-
-		cv2.imshow('frame', frame)
-
-		k = cv2.waitKey(1)
-		if k == 27:
-			break
-
-	cap.release()
-	cv2.destroyAllWindows()
-	return False
-
-
-# cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
-# faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
-
-# while True:
-# 	ret, frame = cap.read()
-# 	if ret == False: break
-# 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-# 	auxFrame = gray.copy()
-
-# 	faces = faceClassif.detectMultiScale(gray,1.3,5)
-
-# 	for (x,y,w,h) in faces:
-# 		rostro = auxFrame[y:y+h,x:x+w]
-# 		rostro = cv2.resize(rostro,(150,150),interpolation= cv2.INTER_CUBIC)
-# 		result = face_recognizer.predict(rostro)
-
-# 		cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
-
-# 		# LBPHFace
-# 		if result[1] < 70:
-# 			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
-# 			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-# 		else:
-# 			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
-# 			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
-		
-# 	cv2.imshow('frame',frame)
-# 	k = cv2.waitKey(1)
-# 	if k == 27:
-# 		break
-
-# cap.release()
-# cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
+    return False
